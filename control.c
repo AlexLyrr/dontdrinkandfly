b1a4f8b619ed9cf694b579bc72a9204918919d16
Roy <r.j.a.blokker@student.tudelft.nl>
2018-05-02 09:04:20 +0200

    Adjustments to motor for input values

--- control.c
+++ control.c
@@ -13,7 +13,7 @@
 #include "in4073.h"
 
 void update_motors(void)
-{					
+{
 	motor[0] = ae[0];
 	motor[1] = ae[1];
 	motor[2] = ae[2];
@@ -26,5 +26,22 @@ void run_filters_and_control()
 	// control loops and/or filters
 
 	// ae[0] = xxx, ae[1] = yyy etc etc
+	//controlLift: 0-1000
+	//controlPitch, controlRoll, controlYaw: 0-180
+	uint16_t adjust = 10000;
+	uint32_t controlPitch = adjust * state.controlPitch;
+	uint32_t controlRoll = adjust * state.controlRoll;
+	uint32_t controlYaw = adjust * state.controlYaw;
+	uint32_t controlLift = state.controlLift;
+	ae[0] = (((controlPitch+controlYaw)/180) * controlLift)/adjust;
+	ae[1] = (((controlRoll + (180*adjust-controlYaw))/180) * controlLift)/adjust;
+	ae[2] = ((((180*adjust - controlPitch)+controlYaw)/180) *controlLift)/adjust;
+	ae[3] = ((((180*adjust - controlRoll) + (180*adjust - controlYaw))/180) * controlLift)/adjust;
+	for (int i = 0; i<4 ; i++){
+		if (ae[i] > 1000){
+			ae[i] = 1000;
+		}
+	}
+
 	update_motors();
 }
