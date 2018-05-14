bcb19623ada851b000e75482c3a28a7a92b9c39c
Roy Blokker <r.j.a.blokker@student.tudelft.nl>
2018-05-14 15:53:45 +0200

    Small change

--- pc_terminal/joystick.c
+++ pc_terminal/joystick.c
@@ -58,7 +58,7 @@ void checkJoystick(struct pcState *pcState){
             if (jTemp > 60800) {
               pcState->jThrottleValue = 0;
             } else{
-              pcState->jThrottleValue = log(60801-jTemp)/log(1.0111)
+              pcState->jThrottleValue = log(60801-jTemp)/log(1.0111);
             }
             break;
           default:
