70c4abecbcd9a2e25c52f3e7ea22aefe546ee03a
Alex Lyr <alex_gfd@hotmail.com>
2018-05-16 15:29:21 +0200

    keyboard values new limits

--- pc_terminal/pc_terminal.c
+++ pc_terminal/pc_terminal.c
@@ -291,7 +291,7 @@ void checkInput(char c, struct pcState *pcState)
 				{
 					case 'A':
 						pcState->escPressed = false;
-						if (pcState->pitchValue > 0 && (pcState->mode == 2 || pcState->mode == 6  || pcState->mode == 5)){
+						if (pcState->pitchValue > 75 && (pcState->mode == 2 || pcState->mode == 6  || pcState->mode == 5)){
 							pcState->upPressed = true;
 							pcState->pitchValue -= 1;
 						}
@@ -299,7 +299,7 @@ void checkInput(char c, struct pcState *pcState)
 						break;
 					case 'B':
 						pcState->escPressed = false;
-						if (pcState->pitchValue < 180 && (pcState->mode == 2 || pcState->mode == 6  || pcState->mode == 5)){
+						if (pcState->pitchValue < 105 && (pcState->mode == 2 || pcState->mode == 6  || pcState->mode == 5)){
 							pcState->downPressed = true;
 							pcState->pitchValue += 1;
 						}
@@ -307,7 +307,7 @@ void checkInput(char c, struct pcState *pcState)
 						break;
 					case 'C':
 						pcState->escPressed = false;
-						if (pcState->rollValue > 0 && (pcState->mode == 2 || pcState->mode == 6  || pcState->mode == 5)){
+						if (pcState->rollValue > 75 && (pcState->mode == 2 || pcState->mode == 6  || pcState->mode == 5)){
 							pcState->rightPressed = true;
 							pcState->rollValue -= 1;
 						}
@@ -315,7 +315,7 @@ void checkInput(char c, struct pcState *pcState)
 						break;
 					case 'D':
 						pcState->escPressed = false;
-						if (pcState->rollValue < 180 && (pcState->mode == 2 || pcState->mode == 6  || pcState->mode == 5)){
+						if (pcState->rollValue < 105 && (pcState->mode == 2 || pcState->mode == 6  || pcState->mode == 5)){
 							pcState->leftPressed = true;
 							pcState->rollValue += 1;
 						}
@@ -360,7 +360,7 @@ void checkInput(char c, struct pcState *pcState)
 			pcState->mode = 8;
 			break;
 		case 'a':
-			if (pcState->liftValue < 1000 && (pcState->mode == 2 || pcState->mode == 4)){
+			if (pcState->liftValue < 1000 && (pcState->mode == 2  || pcState->mode == 4)){
 				pcState->liftValue +=10;
 				pcState->aPressed = true;
      		 }
@@ -788,7 +788,6 @@ void printPcStatusGUI(SRPacket *sPacket){
 
 //@Author Alex Lyrakis
 void logSendPacket(SRPacket sPacket){
-
 	switch(sPacket.payload[0]){
 		case 3:
 			fprintf(Sfile, "Packet number: %hu | Type: %hhu | Abort: %hhu | Roll: %hhu | Pitch: %hhu | Yaw: %hhu | HeightByte1: %hhu | HeightByte0: %hhu",
